5fd49070d6f67c574c992adeed05c192901044fd
yunlong <yunlong@baidu.com>
2014-12-16 13:16:03 +0800

    new sp_no

--- inc/pay_api.inc.php
+++ inc/pay_api.inc.php
@@ -1,8 +1,8 @@
 <?php
 
 final class ordersys_conf {
-     const SP_NO = "1003";
-     const SESSION_SECRET = "3BT8fjxvsJFNDPZmd6QywMYrS7CXEK1i";
+     const SP_NO = "52821";
+     const SESSION_SECRET = "am1eCxTLlT4w34siedL82i02gwyFdEgd";
 
      const ORDER_ADD_PAY_URL = "http://m.baidu.com/lightapp/pay/order/online/add";
      const ORDER_REFUND_URL = "http://m.baidu.com/lightapp/pay/order/refund";
